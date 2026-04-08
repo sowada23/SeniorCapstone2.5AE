0a82061bec974f43b194ae4a4d3a4f6afde6748a
Sora Owada <sowada23@earlham.edu>
2026-04-07 22:49:16 -0400

    Updated checkpoint selection

--- scripts/test.py
+++ scripts/test.py
@@ -6,16 +6,19 @@ sys.path.append(os.path.abspath("."))
 
 from srcs.engine.tester import test
 from srcs.utils.config import load_config
+from srcs.utils.checkpoint import resolve_checkpoint
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="configs/default.yaml")
-    parser.add_argument("--ckpt", type=str, required=True)
+    parser.add_argument("--ckpt", type=str, default=None)
+    parser.add_argument("--output-root", type=str, default="./Output")
     args = parser.parse_args()
 
+    ckpt_path = resolve_checkpoint(args.ckpt, args.output_root)
     cfg = load_config(args.config)
-    test(cfg, args.ckpt)
+    test(cfg, ckpt_path)
 
 
 if __name__ == "__main__":
